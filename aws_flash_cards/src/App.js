9ceba0e9ff257dc98e03f09f102c1883b6ffe418
efrain-vasquez <efrain.vasquez.arias777@gmail.com>
2020-06-01 14:45:43 -0500

    Project: AWS Flash Cards, retrieving data from API with Axios, adding spinner, and moving state up.

---
+++ aws_flash_cards/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./App.css";
+import QuizBar from "./components/QuizBar";
+import FlashCard from "./components/FlashCard";
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cardStyle: "Random",
+      ready: false,
+    };
+  }
+
+  userChoice = (cardStyle) => {
+    this.setState({
+      cardStyle,
+      ready: false,
+    });
+  };
+
+  nowReady = () => {
+    this.setState({
+      ready: true,
+    });
+  };
+
+  render() {
+    console.log(this.state.cardStyle);
+    return (
+      <div className="App align-items-center d-flex">
+        <div className="container">
+          <QuizBar userChoice={this.userChoice} />
+          <FlashCard
+            cardStyle={this.state.cardStyle}
+            nowReady={this.nowReady}
+            ready={this.state.ready}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
